bf5883942ba035328e02cda7ae19043c928592ea
Riccardo Colombo <rikyregna@gmail.com>
2020-07-15 15:17:57 +0200

    working

--- js/script.js
+++ js/script.js
@@ -3,34 +3,38 @@ Chiedere all'utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
 // 1. inserisco parola da prompt e salvo in una var
-var parola = prompt("Orsù, inserisca una parola");
+// 1.1 ciclo do while per verificare che ci sia una parola
+do {
+  var parola = prompt("Orsù, inserisca una parola");
+} while (parola.length <= 0);
 
+// 4. chiamo la funzione (messa qui perchè la creazione è in fondo al codice)
+myPali(parola);
 
 // 2. provo l'algoritmo fuori dall function
-var reverseParola = "";
+// var reverseParola = "";
 
-for (var i = parola.length - 1; i >= 0; i--) {
-  reverseParola += parola[i];
-}
+// for (var i = parola.length - 1; i >= 0; i--) {
+//   reverseParola += parola[i];
+// }
 
-if (reverseParola == parola) {
-  console.log("La parola da te inserita è palindroma");
-} else {
-  console.log("La parola inserita NON è palindroma");
-}
+// if (reverseParola == parola) {
+//   console.log("La parola da te inserita è palindroma");
+// } else {
+//   console.log("La parola inserita NON è palindroma");
+// }
 
 // console.log(reverseParola);
-
 
 // 3. creo la funzione
 function myPali(str) {
   var reverseStr = "";
-  for (var i = str.length - 1;; i >= 0; i--) {
+  for (var i = str.length - 1; i >= 0; i--) {
     reverseStr += str[i];
   }
   if (reverseStr == str) {
-  console.log("La parola da te inserita è palindroma");
-} else {
-  console.log("La parola inserita NON è palindroma");
-}
+    console.log("La parola da te inserita E' PALINDROMA");
+  } else {
+    console.log("La parola inserita NON è palindroma");
+  }
 }
